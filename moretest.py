1e895741dc6cab68aef71a64352d3f5d0e378d39
theluminec <theluminec@gmail.com>
2024-09-24 10:42:33 -0400

    Pruned

--- moretest.py
+++
@@ -1,3 +0,0 @@
-for pr in range(0, 65536, 2048):
-    for p in range(pr, pr+2048):
-        print(p)
--- scan_ip.py
+++
@@ -1,4 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET)
-print(sock.connect(("8.8.8.8", 443)))
